02fe49dcf6368718f29580f64bbf91ba80f2786e
theophiluskibet <kibettheophilus@gmail.com>
2024-05-30 23:07:00 +0300

    setup required modules

---
+++ core/local/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    )
+}
+
+android {
+    namespace = "com.theophiluskibet.caloree.local"
+}
---
+++ feature/onboarding/src/commonMain/kotlin/com/theophiluskibet/caloree/onboarding/Module.kt
@@ -0,0 +1,2 @@
+package com.theophiluskibet.caloree.onboarding
+
