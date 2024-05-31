7232016d78d469d644ff2fcbaf12a7e1344583e8
theophiluskibet <kibettheophilus@gmail.com>
2024-05-31 11:41:00 +0300

    add dtos

---
+++ core/network/src/commonMain/kotlin/com/theophiluskibet/caloree/network/model/CalorieItemsDto.kt
@@ -0,0 +1,13 @@
+package com.theophiluskibet.caloree.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Holds a list of [CalorieItemDto]
+ */
+@Serializable
+data class CalorieItemsDto(
+    @SerialName("items")
+    val calorieItemsDto: List<CalorieItemDto>
+)
