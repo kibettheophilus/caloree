fb7ce81674b69fe802d7e851db77fa4ffc7c609f
theophiluskibet <kibettheophilus@gmail.com>
2024-05-31 14:45:24 +0300

    fix imports

--- core/local/src/commonMain/kotlin/com/theophiluskibet/caloree/local/dao/CaloreeDao.kt
+++ core/local/src/commonMain/kotlin/com/theophiluskibet/caloree/local/dao/CaloreeDao.kt
@@ -3,6 +3,7 @@ package com.theophiluskibet.caloree.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.theophiluskibet.caloree.local.entities.CaloreeEntity
 
 @Dao
@@ -10,12 +11,12 @@ interface CaloreeDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveCalories(list: List<CaloreeEntity>)
 
-    @Query("SELECT * FROM calory_table")
-    suspend fun getCalories(): List<CaloryEntity>
+    @Query("SELECT * FROM caloree_table")
+    suspend fun getCalories(): List<CaloreeEntity>
 
-    @Query("SELECT * FROM calory_table WHERE name=:name")
-    suspend fun getCalory(name: String): CaloryEntity
+    @Query("SELECT * FROM caloree_table WHERE name=:name")
+    suspend fun getCalory(name: String): CaloreeEntity
 
-    @Query("SELECT * FROM calory_table WHERE name IN(:names)")
-    suspend fun getCaloriesByNames(names: List<String>): List<CaloryEntity>
+    @Query("SELECT * FROM caloree_table WHERE name IN(:names)")
+    suspend fun getCaloriesByNames(names: List<String>): List<CaloreeEntity>
 }
