a97f2238b38f7ba1b7a1304d56105a26c718eda6
theophiluskibet <kibettheophilus@gmail.com>
2024-05-31 14:25:45 +0300

    add CaloreeEntity

---
+++ core/local/src/commonMain/kotlin/com/theophiluskibet/caloree/local/entities/CaloreeEntity.kt
@@ -0,0 +1,21 @@
+package com.theophiluskibet.caloree.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "caloree_entity")
+data class CaloreeEntity(
+    @PrimaryKey(autoGenerate = false)
+    val name: String,
+    val calories: Double,
+    val carbohydratesTotalGrams: Double,
+    val cholesterolMilliGrams: Int,
+    val fatSaturatedGrams: Double,
+    val fatTotalGrams: Double,
+    val fiberGrams: Double,
+    val potassiumMilliGrams: Int,
+    val proteinGrams: Double,
+    val servingSizeGrams: Double,
+    val sodiumMilliGrams: Int,
+    val sugarGrams: Double,
+)
