1485d179faecf1aeb1941b699bd2e9d188b7ccca
theophiluskibet <kibettheophilus@gmail.com>
2024-05-31 17:39:44 +0300

    add data module dependencies

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -2,6 +2,18 @@ plugins {
     alias(libs.plugins.caloreeMultiplaform)
 }
 
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.network)
+            implementation(projects.core.local)
+
+            implementation(libs.koin.core)
+            implementation(libs.coroutines.core)
+        }
+    }
+}
+
 android {
     namespace = "com.theophiluskibet.caloree.data"
 }
