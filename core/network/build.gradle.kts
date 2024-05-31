749af05ca24a5e236a330dab62408c0699d45811
theophiluskibet <kibettheophilus@gmail.com>
2024-05-31 11:40:33 +0300

    add ktor libs

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,5 +1,16 @@
 plugins {
     alias(libs.plugins.caloreeMultiplaform)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.bundles.ktor)
+
+            implementation(libs.koin.core)
+        }
+    }
 }
 
 android {
