cb2833c9f067abf61f9d75c737e1f918140ff9f3
theophiluskibet <kibettheophilus@gmail.com>
2024-06-01 16:18:25 +0300

    lint

--- composeApp/src/androidMain/kotlin/com/theophiluskibet/caloree/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/theophiluskibet/caloree/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.theophiluskibet.caloree
 
-import com.theophiluskibet.caloree.di.App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
