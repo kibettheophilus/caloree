9297943af838d04a0c07fa544c53929f7d5ba83d
theophiluskibet <kibettheophilus@gmail.com>
2024-06-02 00:17:06 +0300

    update DetailsUiState

--- feature/details/src/commonMain/kotlin/com/theophiluskibet/calorees/details/utils/DetailsUiState.kt
+++ feature/details/src/commonMain/kotlin/com/theophiluskibet/calorees/details/utils/DetailsUiState.kt
@@ -1,3 +1,10 @@
 package com.theophiluskibet.calorees.details.utils
 
-class DetailsUiState
+import com.theophilus.kibet.caloree.data.model.Caloree
+
+sealed interface DetailsUiState {
+    data object Default : DetailsUiState
+    data object Loading : DetailsUiState
+    data class Success(val data: Caloree) : DetailsUiState
+    data class Error(val errorMessage: String) : DetailsUiState
+}
