582dcabe6aa2a11cec92eff8a94ea3e407111df4
theophiluskibet <kibettheophilus@gmail.com>
2024-06-02 00:17:25 +0300

    setup details module di

--- feature/details/src/commonMain/kotlin/com/theophiluskibet/calorees/details/di/DetailsModule.kt
+++ feature/details/src/commonMain/kotlin/com/theophiluskibet/calorees/details/di/DetailsModule.kt
@@ -1,7 +1,9 @@
 package com.theophiluskibet.calorees.details.di
 
+import com.theophiluskibet.calorees.details.screens.DetailsViewModel
+import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
-val detailsModule =
-    module {
-    }
+val detailsModule = module {
+    viewModelOf(::DetailsViewModel)
+}
