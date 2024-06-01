ac39ad3d387bc0ac88f266b10cdf265b5e73b228
theophiluskibet <kibettheophilus@gmail.com>
2024-06-01 16:16:47 +0300

    add ksp configs to debug local module

--- core/local/build.gradle.kts
+++ core/local/build.gradle.kts
@@ -23,10 +23,10 @@ android {
 }
 
 dependencies {
-//    add("kspAndroid", libs.androidx.room.compiler)
-//    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
-//    add("kspIosX64", libs.androidx.room.compiler)
-//    add("kspIosArm64", libs.androidx.room.compiler)
+    add("kspAndroid", libs.androidx.room.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    add("kspIosX64", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
     add("kspCommonMainMetadata", libs.androidx.room.compiler)
 }
 
