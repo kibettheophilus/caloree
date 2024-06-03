27f076c7ad0017a34464770351573b9feeff8aca
theophiluskibet <kibettheophilus@gmail.com>
2024-06-03 11:43:12 +0300

    add sample composable test

---
+++ feature/details/src/androidTest/kotlin/com/theophiluskibet/calorees/details/screens/DetailScreenTest.kt
@@ -0,0 +1,27 @@
+package com.theophiluskibet.calorees.details.screens
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import org.junit.Rule
+import org.junit.Test
+
+class DetailScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun shouldShowLoadingOnInitialization() {
+        composeTestRule.setContent {
+            MaterialTheme {
+                DetailScreen(
+                    name = "rice",
+                    onNavigateToBack = {}
+                )
+            }
+            composeTestRule.onNodeWithTag("loading").assertIsDisplayed()
+        }
+    }
+}
