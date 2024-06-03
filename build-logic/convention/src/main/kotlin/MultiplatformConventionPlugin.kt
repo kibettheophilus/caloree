ec2046f046b79fa02fe51f6f923c88588574af7a
theophiluskibet <kibettheophilus@gmail.com>
2024-06-03 11:42:35 +0300

    setup min sdk in convention plugin

--- build-logic/convention/src/main/kotlin/MultiplatformConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/MultiplatformConventionPlugin.kt
@@ -33,6 +33,9 @@ class MultiplatformConventionPlugin : Plugin<Project> {
 
                 extensions.configure<LibraryExtension> {
                     compileSdk = 34
+                    defaultConfig {
+                        minSdk = 24
+                    }
                     compileOptions {
                         sourceCompatibility = JavaVersion.VERSION_11
                         targetCompatibility = JavaVersion.VERSION_11
