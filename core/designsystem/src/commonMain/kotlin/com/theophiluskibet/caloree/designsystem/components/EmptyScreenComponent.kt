2abbd5e2d4e9f16b4a461bc137cc36895550ae19
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 15:57:30 +0300

    add test tag to EmptyScreenComponent

--- core/designsystem/src/commonMain/kotlin/com/theophiluskibet/caloree/designsystem/components/EmptyScreenComponent.kt
+++ core/designsystem/src/commonMain/kotlin/com/theophiluskibet/caloree/designsystem/components/EmptyScreenComponent.kt
@@ -6,10 +6,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun EmptyScreenComponent(text: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier.fillMaxSize().testTag("empty_component"), contentAlignment = Alignment.Center) {
         Text(text = text)
     }
 }
