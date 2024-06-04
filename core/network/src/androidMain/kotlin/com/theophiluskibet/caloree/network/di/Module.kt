8fde2cfa35b38b71c7ef6608fca495315f9e6270
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 15:26:23 +0300

    add comment to network android module

--- core/network/src/androidMain/kotlin/com/theophiluskibet/caloree/network/di/Module.kt
+++ core/network/src/androidMain/kotlin/com/theophiluskibet/caloree/network/di/Module.kt
@@ -3,6 +3,9 @@ package com.theophiluskibet.caloree.network.di
 import io.ktor.client.engine.android.Android
 import org.koin.dsl.module
 
+/**
+ * Provides andoid http client engine
+ */
 actual fun platformModule() =
     module {
         single { Android.create() }
