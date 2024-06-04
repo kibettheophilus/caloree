759ad8752bad08cf21e4861c265001e86968f6d9
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 14:23:44 +0300

    provide android http client

---
+++ core/network/src/androidMain/kotlin/com/theophiluskibet/caloree/network/di/module.kt
@@ -0,0 +1,8 @@
+package com.theophiluskibet.caloree.network.di
+
+import io.ktor.client.engine.android.Android
+import org.koin.dsl.module
+
+actual fun platformModule() = module {
+    single { Android.create() }
+}
