216a328e8625387a12b847a3337a2f083a415f2e
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 12:16:40 +0300

    add sample network response

---
+++ core/network/src/commonTest/kotlin/com/theophiluskibet/caloree/network/helpers/Responses.kt
@@ -0,0 +1,22 @@
+package com.theophiluskibet.caloree.network.helpers
+
+val apiResponse = """
+    {
+        "items": [
+            {
+                "name": "rice",
+                "calories": 127.4,
+                "serving_size_g": 100.0,
+                "fat_total_g": 0.3,
+                "fat_saturated_g": 0.1,
+                "protein_g": 2.7,
+                "sodium_mg": 1,
+                "potassium_mg": 42,
+                "cholesterol_mg": 0,
+                "carbohydrates_total_g": 28.4,
+                "fiber_g": 0.4,
+                "sugar_g": 0.1
+            }
+        ]
+    }
+""".trimIndent()
