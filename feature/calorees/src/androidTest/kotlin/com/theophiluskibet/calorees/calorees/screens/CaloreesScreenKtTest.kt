c1434db96ba2a942a4f08a8c92124b23ddac6c52
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 15:57:16 +0300

    add sample test to CaloreesScreenKtTest

---
+++ feature/calorees/src/androidTest/kotlin/com/theophiluskibet/calorees/calorees/screens/CaloreesScreenKtTest.kt
@@ -0,0 +1,28 @@
+package com.theophiluskibet.calorees.calorees.screens
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import org.junit.Rule
+import org.junit.Test
+
+
+class CaloreesScreenKtTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun shouldShowNothing() {
+        composeTestRule.setContent {
+            MaterialTheme {
+                CaloreesScreen(
+                    onNavigateToDetails = {}
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithTag("empty_component").isDisplayed()
+    }
+}
