8d36d2cbb9c62773119e86a6e3bafe27696c612c
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 14:23:33 +0300

    update how httpclient is create

--- core/network/src/commonMain/kotlin/com/theophiluskibet/caloree/network/di/NetworkModule.kt
+++ core/network/src/commonMain/kotlin/com/theophiluskibet/caloree/network/di/NetworkModule.kt
@@ -1,10 +1,8 @@
 package com.theophiluskibet.caloree.network.di
 
-import coil3.network.CacheStrategy
-import coil3.network.NetworkFetcher
-import coil3.network.ktor.asNetworkClient
 import com.theophiluskibet.caloree.network.api.CaloreeApi
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
@@ -16,6 +14,7 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
 /**
@@ -26,43 +25,40 @@ const val BASE_URL = "api.calorieninjas.com/v1"
 
 val networkModule =
     module {
-        single {
-            HttpClient {
-                install(ContentNegotiation) {
-                    json(
-                        Json {
-                            ignoreUnknownKeys = true
-                            isLenient = true
-                        },
-                    )
-                }
+        includes(platformModule())
+        single { createHttpClient(engine = get()) }
+        single { CaloreeApi(httpClient = get()) }
+    }
 
-                install(Logging) {
-                    logger =
-                        object : Logger {
-                            override fun log(message: String) {
-                                // log(message)
-                            }
-                        }
-                    level = LogLevel.ALL
-                }
+expect fun platformModule(): Module
 
-                install(DefaultRequest) {
-                    url {
-                        host = BASE_URL
-                        protocol = URLProtocol.HTTPS
-                    }
-                    header(HttpHeaders.ContentType, ContentType.Application.Json)
-                    header("X-Api-Key", "8/MqBej61B6ALLuEf7cIWg==tJbmaSTQHGZd6wLJ")
+
+fun createHttpClient(engine: HttpClientEngine) = HttpClient(engine = engine) {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                isLenient = true
+            },
+        )
+    }
+
+    install(Logging) {
+        logger =
+            object : Logger {
+                override fun log(message: String) {
+                    // log(message)
                 }
             }
-        }
-
-        single { CaloreeApi(httpClient = get()) }
-        single {
-            NetworkFetcher.Factory(
-                networkClient = { get<HttpClient>().asNetworkClient() },
-                cacheStrategy = { CacheStrategy() },
-            )
-        }
+        level = LogLevel.ALL
     }
+
+    install(DefaultRequest) {
+        url {
+            host = BASE_URL
+            protocol = URLProtocol.HTTPS
+        }
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+        header("X-Api-Key", "8/MqBej61B6ALLuEf7cIWg==tJbmaSTQHGZd6wLJ")
+    }
+}
