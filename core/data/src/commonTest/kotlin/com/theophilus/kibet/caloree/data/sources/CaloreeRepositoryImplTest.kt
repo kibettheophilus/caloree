aca0a2e77b51ca25adeaa1ff5db1683ea14ac4f3
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 15:57:01 +0300

    generate CaloreeRepositoryImplTest

---
+++ core/data/src/commonTest/kotlin/com/theophilus/kibet/caloree/data/sources/CaloreeRepositoryImplTest.kt
@@ -0,0 +1,20 @@
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class CaloreeRepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun searchCalories() {
+    }
+}
