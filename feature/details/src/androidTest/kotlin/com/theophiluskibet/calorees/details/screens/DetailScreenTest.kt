9646de2cb50e4e6280f0bcd4211646b253dd8cc2
theophiluskibet <kibettheophilus@gmail.com>
2024-06-04 15:40:57 +0300

    update DetailScreenTest

--- feature/details/src/androidTest/kotlin/com/theophiluskibet/calorees/details/screens/DetailScreenTest.kt
+++ feature/details/src/androidTest/kotlin/com/theophiluskibet/calorees/details/screens/DetailScreenTest.kt
@@ -4,9 +4,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.theophiluskibet.calorees.details.utils.DetailsUiState
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class DetailScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -15,9 +19,8 @@ class DetailScreenTest {
     fun shouldShowLoadingOnInitialization() {
         composeTestRule.setContent {
             MaterialTheme {
-                DetailScreen(
-                    name = "rice",
-                    onNavigateToBack = {},
+                DetailScreenContent(
+                    caloreeUiState = DetailsUiState.Loading
                 )
             }
             composeTestRule.onNodeWithTag("loading").assertIsDisplayed()
